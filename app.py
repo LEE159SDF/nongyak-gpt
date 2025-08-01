f8014a45177a929db4636ddcb760a6e7ded7e2da
Sanghee Lee <sanghee@example.com>
2025-08-01 17:56:27 +0900

    하단 안내 문구 수정

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ elif st.session_state.page == 'chat':
     st.image("채팅화면.png", use_container_width=True)
     st.markdown("### <span style='color:red; font-size:36px;'>말씀해주세요. 듣고 있습니다</span>", unsafe_allow_html=True)
     st.markdown("### 👉 [🔗 여기 클릭하여 상담 계속하기](https://chatgpt.com/g/g-688c7dbe0a0081919635c927bf1597d6-nongyag-gpt/)", unsafe_allow_html=True)
-    st.info("※ 맞춤형 상담이 어려운 점 양해 바랍니다.")
+    st.info("※ 테스트페이지 입니다. 링크를 클릭하여 상담을 진행해 주세요.")
